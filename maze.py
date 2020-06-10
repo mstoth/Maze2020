da889f98fa08758a45cec0eab0a8154a3f1e16e7
Michael Toth <michael@virtualpianist.com>
2020-06-10 03:46:32 -0400

    test 2, check for screen

--- maze.py
+++ maze.py
@@ -1,12 +1,20 @@
 import unittest
+import turtle
 
 class MazeTests(unittest.TestCase):
     def testItExists(self):
         m=Maze() # this should fail
+    def testScreen(self):
+        m=Maze()
+        self.assertTrue(type(m.screen)==turtle._Screen,"No Screen!")
+        
 
 class Maze():
     """ This class creates a random maze """
-    
+    def __init__(self):
+        self.screen=turtle._Screen()
+
+      
 if __name__=="__main__":
     unittest.main()
 
