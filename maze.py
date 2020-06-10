6cebb95ca30eed26146cf84869a5585b3c927b8f
Michael Toth <michael@virtualpianist.com>
2020-06-10 05:38:38 -0400

    step 5, add size test and refactor

--- maze.py
+++ maze.py
@@ -1,6 +1,6 @@
 import unittest
 import turtle
-
+SIZE=400
 class MazeTests(unittest.TestCase):
     def setUp(self):
         self.m=Maze()
@@ -10,6 +10,9 @@ class MazeTests(unittest.TestCase):
         self.assertTrue(type(self.m.turtle)==turtle.Turtle)
     def testBackground(self):
         self.assertTrue(self.m.s.bgcolor()=='blue')
+    def testSize(self):
+        self.assertTrue(self.m.s.window_width()==SIZE)
+        self.assertTrue(self.m.s.window_height()==SIZE)
 
 class Maze():
     """ This class creates a random maze """
@@ -17,6 +20,7 @@ class Maze():
         self.s=turtle.getscreen()
         self.turtle = turtle.Turtle()
         self.s.bgcolor('blue')
+        self.s.setup(SIZE,SIZE)
         
 if __name__ == "__main__":
     unittest.main()
