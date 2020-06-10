336975b2a22ddffe2b0d2083add8c95059035cb4
Michael Toth <michael@virtualpianist.com>
2020-06-10 02:11:02 -0400

    first commit

---
+++ maze.py
@@ -0,0 +1,12 @@
+import unittest
+
+class MazeTests(unittest.TestCase):
+    def testItExists(self):
+        m=Maze() # this should fail
+
+class Maze():
+    """ This class creates a random maze """
+    
+if __name__=="__main__":
+    unittest.main()
+
