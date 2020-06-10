84761ea3f0fd35ac4bb3807ad1dee0584698c118
Michael Toth <michael@virtualpianist.com>
2020-06-10 04:42:05 -0400

    test 3 checks for turtle

--- maze.py
+++ maze.py
@@ -6,15 +6,16 @@ class MazeTests(unittest.TestCase):
         m=Maze() # this should fail
     def testScreen(self):
         m=Maze()
-        self.assertTrue(type(m.screen)==turtle._Screen,"No Screen!")
-        
+        self.assertTrue(type(m.s)==turtle._Screen,"No Screen!")
+    def testTurtle(self):
+        m=Maze()
+        self.assertTrue(type(m.turtle)==turtle.Turtle)
 
 class Maze():
     """ This class creates a random maze """
     def __init__(self):
-        self.screen=turtle._Screen()
+        self.s=turtle.getscreen()
+        self.turtle = turtle.Turtle()
 
-      
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
-
